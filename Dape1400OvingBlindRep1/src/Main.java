55612508e8d6b5f170305d82ab896466f3c7a226
abdihaliworks-hub <abdi.h.aliworks@gmail.com>
2025-12-14 12:10:41 +0100

    Dape1400, reconsturcted. grade level A design.

--- Dape1400OvingBlindRep1/src/Main.java
+++ Dape1400OvingBlindRep1/src/Main.java
@@ -22,16 +22,20 @@ public class Main {
         int neg = 0;
         int sum = 0;
         int largest = ints[0];
-        for (int anInt : ints) {
-            if (anInt < 0) neg++;
-            if (anInt > largest) largest = anInt;
-            if (anInt % number == 0) System.out.println(anInt + " Is divisable by " + number);
-            sum += anInt;
-
+        if(ints==null || ints.length==0) {
+            System.out.println("Array is empty or null");
         }
-        System.out.println("There are " + neg + " smaller than 0");
-        System.out.println("The largest number is" + largest);
-        System.out.println("The sum of the numbers in the arrays is " + sum);
+
+            for (int anInt : ints) {
+                if (anInt < 0) neg++;
+                if (anInt > largest) largest = anInt;
+                if (anInt % number == 0|| number!=0 ) System.out.println(anInt + " Is divisable by " + number);
+                sum += anInt;
+
+            }
+            System.out.println("There are " + neg + " smaller than 0");
+            System.out.println("The largest number is" + largest);
+            System.out.println("The sum of the numbers in the arrays is " + sum);
 
 
     }
@@ -41,17 +45,18 @@ public class Main {
     //og vi ikke trenger å instansiere nye objekter hver gang vi bruker den.
     //metoden bør være publc, dersom vi ikke har noe som kan overskrives, eller verdt å beskytte.
     public static double stringConversion(String teller, String nevner) {
-        if (teller.isEmpty() || nevner.isEmpty() || nevner.equals("0")) return -1;
-        double n=0;
-        double t=0;
-        double result= t/n;
-        if (teller.matches("\\d+") || nevner.matches("\\d+")) {
-t = Integer.parseInt(teller);
- n =Integer.parseInt(nevner);
+        if (teller==null || nevner==null ) return -1;
 
-return result;
-        } else return -1;
 
+        if (!teller.matches("\\d+") || !nevner.matches("\\d+")) {
+            return -1;
+        }
+       int t = Integer.parseInt(teller);
+        int n = Integer.parseInt(nevner);
+        if (n==0)return-1;
+        else
+
+    return t/n;
     }
     public void guessTheAnimal(ArrayList<String> animals){
         Scanner scanner= new Scanner(System.in);
@@ -65,19 +70,32 @@ return result;
          switch(choice){
              case "a":
                  System.out.println("Skriv inn dyret du satser på");
-                 String guess = scanner.nextLine();
-                 for(String g : animals){
-                     if( g.contains(guess)){
-                         System.out.println("Gratulerer! riktig gjettet");
-                         break;
-                     }  else System.out.println("Desverre feil gjettet, prøv igjen!");
+                 String guess = scanner.nextLine().toLowerCase();
+                 if(guess.isEmpty()){
+                     System.out.println("Kan ikke være tom streng!");
                      break;
-
                  }
+                 boolean found=false;
+                 for(String g : animals){
+                     if( g.toLowerCase().contains(guess)){
+                         System.out.println("Gratulerer! riktig gjettet");
+                         found=true;
+                         break;
+
+                     }
+
+                 } if(found){
+                     System.out.println("Gratulerer! riktig gjettet");}
+                 else System.out.println("Feil gjettet, prøv igjen!");
+                 break;
+
+
              case "b":
                  System.out.println("Ha det bra!");
                  break;
              default: System.out.println("Skriv inn valid input takk!!");
+             break;
+
          }
 
 
